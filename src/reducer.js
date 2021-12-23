8711bd42aefc14781830c9f60888d6e65c35876b
MariiaUlankina <ulankina.masha@gmail.com>
2021-12-23 16:56:37 +0200

    added function reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,25 @@
+function capitalize(string) {
+  let arr = string.split(' ');
+  let newArr = arr.map(function(item){
+      let word =item.slice(1);
+      let result = item[0].toUpperCase();
+      return `${result}${word}`
+  })
+  return newArr.join(' ')
+}
+
 function reducer(input, action) {
   // Ваше решение тут
+  switch (action){
+    case 'upperCase': console.log(string.toUpperCase());
+      break;
+    case 'lowerCase': console.log(string.toLowerCase());
+      break;
+    case 'capitalize': console.log(capitalize(string));
+      break;
+
+  }
+
 }
 
 module.exports = reducer;
